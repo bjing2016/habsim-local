013ac2802e7db53ba46e67d45cae0ec46b66a2b7
bjing2016 <bjing2016@users.noreply.github.com>
2019-08-07 16:56:02 -0700

    webplot circles, click for coordinates and elevation

--- habsim/ioutil.py
+++ habsim/ioutil.py
@@ -28,7 +28,7 @@ def get_html_marker_string(lat, lon, label, title):
         marker.setMap(map);
     '''
 
-def get_html_path_string(path_cache, color, counter, weight=2):
+def get_html_path_string(path_cache, color, counter, opacity=1.0, weight=2):
     '''
     Internal method. Do not use.
     '''
@@ -41,12 +41,42 @@ def get_html_path_string(path_cache, color, counter, weight=2):
           path: flightPlanCoordinates"""+str(counter)+""",
           geodesic: true,
           strokeColor: '""" + color + """',
-          strokeOpacity: 1.0,
+          strokeOpacity: """ + str(opacity) + """,
           strokeWeight: """ + str(weight) + """
         });
 
         flightPath"""+str(counter) + """.setMap(map);"""
     return string
+
+def get_circle_string(lat, lon, radius, content, strokeColor, strokeOpacity, strokeWeight, fillColor, fillOpacity):
+    string = """
+    var circle = new google.maps.Circle({
+        strokeColor: '""" + strokeColor + """',
+        strokeOpacity: """ + str(strokeOpacity) +""",
+        strokeWeight: """ + str(strokeWeight) + """,
+        fillColor: '""" + fillColor + """',
+        fillOpacity: """ + str(fillOpacity) + """,
+        map: map,
+        center: {lat: """ + str(lat) + """, lng: """ + str(lon) + """},
+        radius: """+str(radius)+''',
+        clickable: true
+    });
+    '''
+    if content is None: return string
+    string+= '''
+    var infowindow = new google.maps.InfoWindow({
+        content: "'''+ content + '''"
+    });
+    circle.addListener("mouseover", function () {
+        infowindow.setPosition(circle.getCenter());
+        infowindow.open(map);
+    });
+    circle.addListener("mouseout", function () {
+        infowindow.close(map);
+    });
+    '''
+    return string
+
 
 default_colors = ["#000000", "#FF0000", "#008000", "#800000", "#808000"]
 class WebPlot:
@@ -57,7 +87,7 @@ class WebPlot:
         self.pathstring = ""
         self.counter = 0
 
-    def add(self, trajectories, colors = default_colors, weight=2):
+    def add(self, trajectories, colors = default_colors, opacity=1.0, weight=2):
         '''
         Adds a list of trajectories. If list is longer than 5, colors must be specified.
         Whenever this method is called, the list of colors is used from the beginning.
@@ -67,7 +97,7 @@ class WebPlot:
             raise Exception("Longest trajectory longer than number of colors.")
         
         for i in range(len(trajectories)):
-            self.pathstring += get_html_path_string(trajectories[i], colors[i], self.counter, weight)
+            self.pathstring += get_html_path_string(trajectories[i], colors[i], self.counter, opacity, weight)
             self.counter += 1
 
     def marker(self, lat, lon, label="", title=""):
@@ -76,12 +106,19 @@ class WebPlot:
         '''
         self.pathstring += get_html_marker_string(lat, lon, label, title)
 
+    def circle(self, lat, lon, radius, content=None, strokeColor='#FF0000', strokeOpacity=1, strokeWeight=1, fillColor='#FF0000', fillOpacity=0.35):
+        '''
+        Adds a circle with content shown upon mouseover and radius approximately in meters.
+        '''
+        self.pathstring += get_circle_string(lat, lon, radius, content, strokeColor, strokeOpacity, strokeWeight, fillColor, fillOpacity)
+        
+
     def origin(self, lat, lon, zoom=7):
         '''
         Sets the origin of the plot. Must be called.
         '''
-        self.slat = lat
-        self.slon = lon
+        self.lat = lat
+        self.lon = lon
         self.zoom = zoom
     
     def save(self, name):
@@ -89,12 +126,10 @@ class WebPlot:
         Writes file to disk.
         '''
         f = open(name, "w")
-        f.write(part1 + str(self.slat) + "," + str(self.slon) + "),zoom:" + str(self.zoom))
+        f.write(part1 + str(self.lat) + "," + str(self.lon) + "),zoom:" + str(self.zoom))
         f.write(part2)
         f.write(self.pathstring)
-        f.write(part3short)
-        f.write(part4 + part5)
-        f.close()
+        f.write(part3)
 
 #########
 
@@ -113,10 +148,16 @@ part1 = '''
                 margin: 0;
                 padding: 0;
             }
+        .small {
+            position: absolute;
+            top: 20px;
+            right: 60px;
+            }
         </style> 
     </head>
     <body>
-        <div id="map" style="float: left;"></div>       
+        <div id="map" style="float: left;"></div>
+        <div class="small"><span id="text"></span><span id="elev"></span></div>       
         <!-- bring in the google maps library -->
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
         <script type="text/javascript">
@@ -152,21 +193,27 @@ part2 = ''' ,
             }));
 ''' 
 
-part3short = '''
+part3 = '''
+        function displayCoordinates(pnt) {
+            var lat = pnt.lat();
+            lat = lat.toFixed(4);
+            var lng = pnt.lng();
+            lng = lng.toFixed(4);
+            elev = getElev(lat, lng)
+            document.getElementById("text").textContent = String(lat) + "," + String(lng) + " "
+        }
+        function getElev(lat, lng) {
+            fetch("https://predict.stanfordssi.org/elev?lat=" + lat + "&lon=" + lng).then(res => res.json()).then((result) => {
+                document.getElementById("elev").textContent = result + " m"
+            })
+        }
         </script>
     <div id="map_canvas" style="width:80%; height:100%"></div>
     <div id="info" style="padding-left:2ch; padding-right: 2ch">
 
       <div>
-   
-'''
-
-part4 = '''
-
       </div>
     </div>
-    '''
-part5 = '''
 </body>
 </html>
 '''
